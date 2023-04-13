e394eaca6e1cbcd1d3fc72bafc6b8fcea52e9699
rk <b969853447@qq.com>
2023-04-13 20:19:55 +0800

    fix

--- src/main/java/com/reach/configure/WebConfigure.java
+++ src/main/java/com/reach/configure/WebConfigure.java
@@ -1,6 +1,9 @@
 package com.reach.configure;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +17,10 @@ public class WebConfigure implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseSuffixPatternMatch(true);
         configurer.setUseTrailingSlashMatch(true);
+    }
+
+    @Bean(name="multipartResolver")
+    public MultipartResolver multipartResolver(){
+        return new CommonsMultipartResolver();
     }
 }
